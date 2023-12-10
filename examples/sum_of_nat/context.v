e2c98dfa983e5183b57f5a2784d9373d35e6d2b9
mtarunpr <m.tarunprasad@gmail.com>
2023-12-09 19:06:40 -0500

    Remove incorrect example

--- examples/sum_of_nat/context.v
+++
@@ -1,8 +0,0 @@
-Require Import Coq.Lists.List.
-Import ListNotations.
-
-Fixpoint sum_natural_numbers (n : nat) : nat :=
-  match n with
-  | O => 0
-  | S n' => n + sum_natural_numbers n'
-  end.
--- examples/sum_of_nat/theorem.v
+++
@@ -1,2 +0,0 @@
-Theorem sum_of_natural_numbers : forall n : nat,
-  2 * (sum_natural_numbers n) = n * (n + 1).

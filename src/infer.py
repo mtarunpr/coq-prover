8c0128e7ea2b99f7090cd570775750f7d47e6527
mtarunpr <m.tarunprasad@gmail.com>
2024-03-14 05:40:20 -0400

    Modularize gpt + local model

--- src/infer.py
+++ src/infer.py
@@ -1,3 +1,6 @@
+# Caching process ported from https://github.com/metareflection/gpt-call
+# LocalModel inference ported from https://github.com/metareflection/llm-scripts
+
 import torch
 from transformers import (
     AutoModelForCausalLM,
@@ -6,48 +9,150 @@ from transformers import (
     TextStreamer,
 )
 import csv
-import random
 from peft import PeftModel
+from tenacity import retry, stop_after_attempt, wait_random_exponential
+from joblib import Memory
+from openai import OpenAI
+from openai.types.chat import ChatCompletionMessageParam
+from typing import Iterable
+from coq import Theorem
+import os
+import re
+
+memory = Memory("cachegpt", verbose=0)
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
-model_checkpoint = "./checkpoints/checkpoint-500"
+class BaseLLM:
+    def ask(self, messages: Iterable[ChatCompletionMessageParam]):
+        raise NotImplementedError
 
-base_model_name = "Phind/Phind-CodeLlama-34B-v2"
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float16,
-)
-base_model = AutoModelForCausalLM.from_pretrained(
-    base_model_name,
-    quantization_config=bnb_config,
-    device_map="auto",
-    trust_remote_code=True,
-    use_auth_token=True,
-)
+    def ask_for_proof(self, theorem: Theorem, prev_attempt_error_msg=None):
+        """
+        Attempts to prove a theorem using GPT-4. Stores the resulting proof in theorem.proof.
+        """
+        messages = [
+            {
+                "role": "system",
+                "content": "You are an automated theorem prover that can prove theorems and lemmas in Coq. Your entire response must be valid Coq code. You should explain your reasoning (what the proof steps are attempting to do), but only in comments inside the Coq code. The following messages will all consist of a theorem statement (possibly preceded by necessary definitions, imports, etc.), and your response must be a valid Coq proof of that theorem. Your response must be in this format: ```coq\n Proof.\n<proof>. Qed.\n```. Remember: do not add any other text besides Coq code and do not repeat any imports, definitions, lemmas, etc. provided in the prompt.",
+            },
+            {
+                "role": "user",
+                "content": theorem.get_preamble_string() + "\n\n" + str(theorem),
+            },
+        ]
+        if prev_attempt_error_msg is not None:
+            messages += [
+                {
+                    "role": "assistant",
+                    "content": "```coq" + theorem.get_proof_string() + "\n```",
+                },
+                {
+                    "role": "user",
+                    "content": "This is incorrect; Coq produced the following error message: "
+                    + prev_attempt_error_msg
+                    + "\n\nPlease try again.",
+                },
+            ]
+        response = self.ask(messages, self.model_name)
+        proof_contents = response.split("Proof.")[1].split("Qed.")[0]
+        proof_str = "Proof.\n" + proof_contents + "\nQed."
+        theorem.proof = re.findall(r"(.+?\.)(?:\s+|$)", proof_str, flags=re.DOTALL)
 
-tokenizer = AutoTokenizer.from_pretrained(base_model_name, trust_remote_code=True)
-tokenizer.pad_token = tokenizer.eos_token
+    def create_lemma_name(self, lemma_str: str, suffix: str):
+        messages = [
+            {
+                "role": "system",
+                "content": "You are a proof helper for Coq that can come up with descriptive names for lemmas and theorems based on the statement of the proposition. Specifically, Replace `helper_lemma` with a better, more descriptive, name for the following lemma(s) in Coq. Your entire response must be valid Coq code. Your response must be in this format: ```coq\nLemma <new_lemma_name> : <lemma_statement>.\n```.",
+            },
+            {"role": "user", "content": lemma_str},
+        ]
+        response = self.ask(messages, self.model_name)
+        new_lemma_name = response.split("Lemma ")[1].split(":")[0].strip()
+        return new_lemma_name + "_" + suffix
 
-model = base_model
-model = PeftModel.from_pretrained(base_model, model_checkpoint)
 
-streamer = TextStreamer(tokenizer)
+class GPT(BaseLLM):
+    def __init__(self, model_name: str):
+        self.model_name = model_name
+
+    @retry(wait=wait_random_exponential(min=10, max=30), stop=stop_after_attempt(25))
+    def _generate(self, messages):
+        print("Prompting GPT... model_name=" + self.model_name)
+        return client.chat.completions.create(model=self.model_name, messages=messages)
+
+    @memory.cache
+    def ask(self, messages: Iterable[ChatCompletionMessageParam]):
+        response = self._generate(messages)
+        return response.choices[0].message.content
 
 
-def generate(eval_prompt):
-    model_input = tokenizer(eval_prompt, return_tensors="pt").to("cuda")
-
-    model.eval()
-
-    r = None
-    with torch.no_grad():
-        r = tokenizer.decode(
-            model.generate(**model_input, streamer=streamer, max_new_tokens=500)[0],
-            skip_special_tokens=True,
+class LocalModel(BaseLLM):
+    def __init__(self, model_name: str, model_checkpoint: str | None = None):
+        self.model_checkpoint = model_checkpoint
+        self.base_model_name = model_name
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.float16,
         )
-        print(r)
-    return r
+        base_model = AutoModelForCausalLM.from_pretrained(
+            self.base_model_name,
+            quantization_config=bnb_config,
+            device_map="auto",
+            trust_remote_code=True,
+            use_auth_token=True,
+        )
+
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.base_model_name, trust_remote_code=True
+        )
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+
+        if self.model_checkpoint is None:
+            self.model = base_model
+        else:
+            self.model = PeftModel.from_pretrained(base_model, self.model_checkpoint)
+
+        self.streamer = TextStreamer(self.tokenizer)
+
+    def _generate(self, prompt):
+        print(
+            "Prompting LocalMode... model_name="
+            + self.model_name
+            + (" model_checkpoint=" + self.model_checkpoint)
+            if self.model_checkpoint is not None
+            else ""
+        )
+
+        model_input = self.tokenizer(prompt, return_tensors="pt").to("cuda")
+
+        self.model.eval()
+
+        r = None
+        with torch.no_grad():
+            r = self.tokenizer.decode(
+                self.model.generate(
+                    **model_input, streamer=self.streamer, max_new_tokens=500
+                )[0],
+                skip_special_tokens=True,
+            )
+        return r
+
+    @memory.cache
+    def ask(self, messages: Iterable[ChatCompletionMessageParam]):
+        prompt = ""
+        for message in messages:
+            if message["role"] == "system":
+                prompt += "### System Prompt\n" + message["content"] + "\n\n"
+            elif message["role"] == "user":
+                prompt += "### User Message\n" + message["content"] + "\n\n"
+            elif message["role"] == "assistant":
+                prompt += "### Assistant\n" + message["content"] + "\n\n"
+            else:
+                raise ValueError("Invalid message role: " + message["role"])
+            
+        return self._generate(prompt)
 
 
 if __name__ == "__main__":
@@ -59,5 +164,9 @@ if __name__ == "__main__":
             # if random.random() < 0.9:
             #     continue
             prompt = f"Given the following context and theorem statement in Coq, generate a proof.\n\n#### Context\n{example['preamble']}\n\n#### Theorem\n{example['theorem']}\n\n#### Proof\n"
-            generate(prompt)
+            llm = LocalModel(
+                "Phind/Phind-CodeLlama-34B-v2",
+                "./checkpoints/checkpoint-500",
+            )
+            print(llm._generate(prompt))
             break
--- src/lemma-prover.py
+++ src/lemma-prover.py
@@ -1,23 +1,19 @@
-from openai import OpenAI
-
 from alectryon.serapi import annotate, Sentence, Text
-from tenacity import retry, stop_after_attempt, wait_random_exponential
-from joblib import Memory
 from contextlib import redirect_stderr
 from dotenv import load_dotenv
-import os
 import re
 import argparse
 from coq import Theorem
 from coq_parser import parse_file
 from pathlib import Path
+from infer import GPT, LocalModel
 
 load_dotenv()
 
-memory = Memory("cachegpt", verbose=0)
-
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-MODEL = "gpt-4-1106-preview"
+LLM_TYPE = "GPT"  # GPT | LocalModel
+GPT_MODEL_NAME = "gpt-4-1106-preview"
+LOCAL_MODEL_NAME = "Phind/Phind-CodeLlama-34B-v2"
+LOCAL_MODEL_CHECKPOINT = None
 MAX_LEMMA_RETRIES = 5
 MAX_LEMMA_DEPTH = 20
 
@@ -26,54 +22,19 @@ warning_indicators = [
     "Loading ML file",
 ]
 
-
-# Caching process ported from https://github.com/metareflection/gpt-call
-@retry(wait=wait_random_exponential(min=10, max=30), stop=stop_after_attempt(25))
-def generate(messages, model):  # "gpt-3.5-turbo", "gpt-4"
-    print("calling GPT... model=" + model)
-    return client.chat.completions.create(model=model, messages=messages)
-
-
-@memory.cache
-def ask(messages, model):
-    response = generate(messages, model)
-    return response.choices[0].message.content
-
-
-def prove_using_gpt(theorem: Theorem, model, prev_attempt_error_msg=None):
-    """
-    Attempts to prove a theorem using GPT-4. Stores the resulting proof in theorem.proof.
-    """
-    messages = [
-        {
-            "role": "system",
-            "content": "You are an automated theorem prover that can prove theorems and lemmas in Coq. Your entire response must be valid Coq code. You should explain your reasoning (what the proof steps are attempting to do), but only in comments inside the Coq code. The following messages will all consist of a theorem statement (possibly preceded by necessary definitions, imports, etc.), and your response must be a valid Coq proof of that theorem. Your response must be in this format: ```coq\n Proof.\n<proof>. Qed.\n```. Remember: do not add any other text besides Coq code and do not repeat any imports, definitions, lemmas, etc. provided in the prompt.",
-        },
-        {
-            "role": "user",
-            "content": theorem.get_preamble_string() + "\n\n" + str(theorem),
-        },
-    ]
-    if prev_attempt_error_msg is not None:
-        messages += [
-            {
-                "role": "assistant",
-                "content": "```coq" + theorem.get_proof_string() + "\n```",
-            },
-            {
-                "role": "user",
-                "content": "This is incorrect; Coq produced the following error message: "
-                + prev_attempt_error_msg
-                + "\n\nPlease try again.",
-            },
-        ]
-    response = ask(messages, model)
-    proof_contents = response.split("Proof.")[1].split("Qed.")[0]
-    proof_str = "Proof.\n" + proof_contents + "\nQed."
-    theorem.proof = re.findall(r"(.+?\.)(?:\s+|$)", proof_str, flags=re.DOTALL)
+if LLM_TYPE == "GPT":
+    llm = GPT(GPT_MODEL_NAME)
+elif LLM_TYPE == "LocalModel":
+    llm = LocalModel(LOCAL_MODEL_NAME, LOCAL_MODEL_CHECKPOINT)
+else:
+    raise ValueError("Invalid model type")
 
 
 def annotate_and_fetch_error(theorem: Theorem):
+    """
+    Annotates the theorem's proof using Alectryon and returns the annotated code
+    fragments as well as the index of the first error, if any (or -1 if no error).
+    """
     first_error_idx = -1
     annotated_code = annotate(
         [
@@ -102,22 +63,13 @@ def annotate_and_fetch_error(theorem: Theorem):
     return annotated_code_fragments, first_error_idx
 
 
-def create_lemma_name(lemma_str: str, suffix):
-    messages = [
-        {
-            "role": "system",
-            "content": "You are a proof helper for Coq that can come up with descriptive names for lemmas and theorems based on the statement of the proposition. Specifically, Replace `helper_lemma` with a better, more descriptive, name for the following lemma(s) in Coq. Your entire response must be valid Coq code. Your response must be in this format: ```coq\nLemma <new_lemma_name> : <lemma_statement>.\n```.",
-        },
-        {"role": "user", "content": lemma_str},
-    ]
-    response = ask(messages, MODEL)
-    new_lemma_name = response.split("Lemma ")[1].split(":")[0].strip()
-    return new_lemma_name + "_" + suffix
-
-
 def proof_state_to_lemma(
-    lemma_name_suffix, hypotheses, conclusion, preamble, context_str
+    lemma_name_suffix: str, hypotheses, conclusion, preamble, context_str: str
 ):
+    """
+    Converts a proof state to a lemma that, if applied, would complete the proof
+    of the current goal.
+    """
     lemma_stmt = ""
     if len(hypotheses) > 0:
         for hypothesis in hypotheses:
@@ -127,7 +79,7 @@ def proof_state_to_lemma(
     lemma_stmt += conclusion
 
     # Replace "helper_lemma" with a better name
-    lemma_name = create_lemma_name(
+    lemma_name = llm.create_lemma_name(
         f"Lemma helper_lemma : {lemma_stmt}", lemma_name_suffix
     )
 
@@ -197,6 +149,9 @@ def recursively_prove_lemma(
     prev_attempt_error_message=None,
     prev_attempt_error_idx=None,
 ):
+    """
+    Recursively attempts to prove a lemma, using the previous attempt's error message.
+    """
     # If a previous attempt had an error, print it
     if prev_attempt_error_message is not None:
         print(f"ERROR MESSAGE IN LEMMA PROOF (FRAGMENT #{prev_attempt_error_idx})")
@@ -210,16 +165,12 @@ def recursively_prove_lemma(
 
     # If this is the first attempt, try to prove the lemma
     if depth == 0:
-        prove_using_gpt(lemma, MODEL)
+        llm.ask_for_proof(lemma)
     # Otherwise, try to prove the lemma again using the previous attempt's error message
     else:
         print(f"LEMMA PROOF IS INVALID. TRYING AGAIN... (ATTEMPT {depth})")
         print()
-        prove_using_gpt(
-            lemma,
-            MODEL,
-            prev_attempt_error_message,
-        )
+        llm.ask_for_proof(lemma, prev_attempt_error_message)
 
     # Print the lemma's proof
     print("GPT PROOF OF LEMMA")
@@ -249,6 +200,11 @@ def recursively_prove_lemma(
 
 
 def check_theorem_proof_and_maybe_reprove_using_lemmas(theorem: Theorem, depth=0):
+    """
+    Checks if the theorem's proof is valid. If not, extracts the proof state
+    before the error and tries to prove that goal separately as a lemma.
+    Does this recursively until either the theorem is proven or the max depth is reached.
+    """
     # Break out of recursion if we've reached the max depth
     if depth > MAX_LEMMA_DEPTH:
         print(f"MAX {theorem.keyword.upper()} ERROR COUNT REACHED. GIVING UP.")
@@ -298,7 +254,7 @@ def check_theorem_proof_and_maybe_reprove_using_lemmas(theorem: Theorem, depth=0
         print(lemma)
         print()
 
-        prove_using_gpt(lemma, MODEL)
+        llm.ask_for_proof(lemma)
         check_theorem_proof_and_maybe_reprove_using_lemmas(lemma, depth + 1)
 
         # Now that we have a valid lemma, we can
@@ -395,10 +351,7 @@ if __name__ == "__main__":
         context_str,
     )
 
-    prove_using_gpt(
-        theorem,
-        MODEL,
-    )
+    llm.ask_for_proof(theorem)
 
     with open(data_dir / "stderr.txt", "w") as f:
         with redirect_stderr(f):
